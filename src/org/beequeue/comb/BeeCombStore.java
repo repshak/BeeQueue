776188bfb0e74fede17547b10807fffbd0072ea8
Sergey Kashperskiy <sk@jgenerator.com>
2013-01-02 23:04:23 -0800

    commit error to test jenkins

--- src/org/beequeue/comb/BeeCombStore.java
+++ src/org/beequeue/comb/BeeCombStore.java
@@ -3,7 +3,7 @@ package org.beequeue.comb;
 import org.beequeue.hash.HashKey;
 
 public interface BeeCombStore {
-	BeeFrame get(String collectionName);
+	Bee Frame get(String collectionName);
 	ContentSource read(HashKey key);
 	HashKey save(ContentSource content);
 }
