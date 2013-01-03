d948cf2d9c215098cbb7ee900c124987dbc73ba1
Sergey Kashperskiy <sk@jgenerator.com>
2013-01-02 23:07:33 -0800

    fo fix jenkins

--- src/org/beequeue/comb/BeeCombStore.java
+++ src/org/beequeue/comb/BeeCombStore.java
@@ -3,7 +3,7 @@ package org.beequeue.comb;
 import org.beequeue.hash.HashKey;
 
 public interface BeeCombStore {
-	Bee Frame get(String collectionName);
+	BeeFrame get(String collectionName);
 	ContentSource read(HashKey key);
 	HashKey save(ContentSource content);
 }
