383da7a336d27d0f252845198e9092724d0cf930
Sergey Kashperskiy <sk@jgenerator.com>
2013-01-07 08:51:04 -0800

    remove extra lines

--- scripts/codegen/RemoveTildas.groovy
+++ scripts/codegen/RemoveTildas.groovy
@@ -4,10 +4,8 @@ package codegen;
 class RemoveTildas {
 
   static void main(args) {
- 	["src","test"].each
- 	{
-	  new File(it).eachFileRecurse 
-	  { 
+ 	["src","test"].each {
+	  new File(it).eachFileRecurse { 
 	    if ( it.name =~ /java~$/ ) {
 	      if( it.delete() ){
 	      	println it
